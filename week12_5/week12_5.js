636f459b18e052bc949ca27d4bd0491c6260e020
angelnini <91250186+angelnini@users.noreply.github.com>
2022-05-20 09:36:28 +0800

    Update week12_5.js

--- week12_5/week12_5.js
+++ week12_5/week12_5.js
@@ -10,12 +10,16 @@ function draw() {
   if (choice==0) rect(50,50,150,2);
   if (choice==1) rect(50,100,150,2);
   if (choice==2) rect(50,150,150,2);
+  if (choice==3) rect(50,200,150,2);
+  if (choice==4) rect(50,250,150,2);
   text("Choice 1",50,50);
   text("Choice 2",50,100);
   text("Choice 3",50,150);
+  text("Choice 4,50,200);
+  text("Choice 5,50,250);
 }
 function mousePressed() {
-    choice = int(random(3));
+    choice = int(random(5));
 // choice = (choice + 1) % 3;
 }
 
